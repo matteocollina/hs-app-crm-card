03a258dac4d434f653184a0b2f3ad6aa718b9ccf
Matteo Collina <collinamatteo@gmail.com>
2021-12-09 22:41:05 +0100

    handled mock data

--- functions/api.js
+++ functions/api.js
@@ -23023,91 +23023,111 @@ router.get("/", (req, res) => {
   res.json({
     "results": [{
       "objectId": 245,
-      "title": "API-22: APIs working too fast",
-      "link": "http://example.com/1",
-      "created": "2016-09-15",
+      "title": "Linea Magazzino",
+      "link": "https://sales.pieri.it/66c27100-9ca6-428f-97dc-95a9c4c0025d_0/index.html",
+      "created": "2021-12-09",
       "priority": "HIGH",
       "project": "API",
-      "reported_by": "msmith@hubspot.com",
-      "description": "Customer reported that the APIs are just running too fast. This is causing a problem in that they're so happy.",
+      "reported_by": "matteo.collina@diemmea.com",
+      "description": "Quote in PE.",
       "reporter_type": "Account Manager",
       "status": "In Progress",
-      "ticket_type": "Bug",
-      "updated": "2016-09-28",
-      "actions": [{
-        "type": "IFRAME",
-        "width": 890,
-        "height": 748,
-        "uri": "https://example.com/edit-iframe-contents",
-        "label": "Edit",
-        "associatedObjectProperties": []
-      }, {
-        "type": "IFRAME",
-        "width": 890,
-        "height": 748,
-        "uri": "https://example.com/reassign-iframe-contents",
-        "label": "Reassign",
-        "associatedObjectProperties": []
-      }, {
-        "type": "ACTION_HOOK",
-        "httpMethod": "PUT",
-        "associatedObjectProperties": [],
-        "uri": "https://example.com/tickets/245/resolve",
-        "label": "Resolve"
-      }, {
-        "type": "CONFIRMATION_ACTION_HOOK",
-        "confirmationMessage": "Are you sure you want to delete this ticket?",
-        "confirmButtonText": "Yes",
-        "cancelButtonText": "No",
-        "httpMethod": "DELETE",
-        "associatedObjectProperties": ["protected_account"],
-        "uri": "https://example.com/tickets/245",
-        "label": "Delete"
-      }]
-    }, {
-      "objectId": 988,
-      "title": "API-54: Question about bulk APIs",
-      "link": "http://example.com/2",
-      "created": "2016-08-04",
-      "priority": "HIGH",
-      "project": "API",
-      "reported_by": "ksmith@hubspot.com",
-      "description": "Customer is not able to find documentation about our bulk Contacts APIs.",
-      "reporter_type": "Support Rep",
-      "status": "Resolved",
-      "ticket_type": "Bug",
-      "updated": "2016-09-23",
+      "ticket_type": "Quote",
+      "updated": "2021-12-10",
       "properties": [{
-        "label": "Resolved by",
+        "label": "Created by",
         "dataType": "EMAIL",
-        "value": "ijones@hubspot.com"
-      }, {
-        "label": "Resolution type",
-        "dataType": "STRING",
-        "value": "Referred to documentation"
-      }, {
-        "label": "Resolution impact",
-        "dataType": "CURRENCY",
-        "value": "94.34",
-        "currencyCode": "GBP"
+        "value": "pierluigi.alexiadis@pieri.it"
       }],
       "actions": [{
         "type": "IFRAME",
         "width": 890,
         "height": 748,
-        "uri": "https://example.com/edit-iframe-contents",
-        "label": "Edit"
-      }, {
-        "type": "CONFIRMATION_ACTION_HOOK",
-        "confirmationMessage": "Are you sure you want to delete this ticket?",
-        "confirmButtonText": "Yes",
-        "cancelButtonText": "No",
-        "httpMethod": "DELETE",
-        "associatedObjectProperties": ["protected_account"],
-        "uri": "https://example.com/tickets/245",
-        "label": "Delete"
-      }]
-    }],
+        "uri": "https://sales.pieri.it/66c27100-9ca6-428f-97dc-95a9c4c0025d_0/document.pdf",
+        "label": "Download PDF",
+        "associatedObjectProperties": []
+      } // {
+      //   "type": "IFRAME",
+      //   "width": 890,
+      //   "height": 748,
+      //   "uri": "https://example.com/reassign-iframe-contents",
+      //   "label": "Reassign",
+      //   "associatedObjectProperties": []
+      // },
+      // {
+      //   "type": "ACTION_HOOK",
+      //   "httpMethod": "PUT",
+      //   "associatedObjectProperties": [],
+      //   "uri": "https://example.com/tickets/245/resolve",
+      //   "label": "Resolve"
+      // },
+      // {
+      //   "type": "CONFIRMATION_ACTION_HOOK",
+      //   "confirmationMessage": "Are you sure you want to delete this ticket?",
+      //   "confirmButtonText": "Yes",
+      //   "cancelButtonText": "No",
+      //   "httpMethod": "DELETE",
+      //   "associatedObjectProperties": [
+      //     "protected_account"
+      //   ],
+      //   "uri": "https://example.com/tickets/245",
+      //   "label": "Delete"
+      // }
+      ]
+    } // {
+    //   "objectId": 988,
+    //   "title": "API-54: Question about bulk APIs",
+    //   "link": "http://example.com/2",
+    //   "created": "2016-08-04",
+    //   "priority": "HIGH",
+    //     "project": "API",
+    //     "reported_by": "ksmith@hubspot.com",
+    //     "description": "Customer is not able to find documentation about our bulk Contacts APIs.",
+    //     "reporter_type": "Support Rep",
+    //     "status": "Resolved",
+    //     "ticket_type": "Bug",
+    //     "updated": "2016-09-23",
+    //     "properties": [
+    //       {
+    //         "label": "Resolved by",
+    //         "dataType": "EMAIL",
+    //         "value": "ijones@hubspot.com"
+    //       },
+    //       {
+    //         "label": "Resolution type",
+    //         "dataType": "STRING",
+    //         "value": "Referred to documentation"
+    //       },
+    //       {
+    //         "label": "Resolution impact",
+    //         "dataType": "CURRENCY",
+    //         "value": "94.34",
+    //         "currencyCode": "GBP"
+    //       }
+    //     ],
+    //     "actions": [
+    //       {
+    //         "type": "IFRAME",
+    //         "width": 890,
+    //         "height": 748,
+    //         "uri": "https://example.com/edit-iframe-contents",
+    //         "label": "Edit"
+    //       },
+    //       {
+    //         "type": "CONFIRMATION_ACTION_HOOK",
+    //         "confirmationMessage": "Are you sure you want to delete this ticket?",
+    //         "confirmButtonText": "Yes",
+    //         "cancelButtonText": "No",
+    //         "httpMethod": "DELETE",
+    //         "associatedObjectProperties": [
+    //           "protected_account"
+    //         ],
+    //         "uri": "https://example.com/tickets/245",
+    //         "label": "Delete"
+    //       }
+    //     ]
+    //   }
+    ],
     "settingsAction": {
       "type": "IFRAME",
       "width": 890,
@@ -23120,7 +23140,7 @@ router.get("/", (req, res) => {
       "width": 890,
       "height": 748,
       "uri": "https://example.com/create-iframe-contents",
-      "label": "Create Ticket"
+      "label": "New Quote"
     }
   });
 });
